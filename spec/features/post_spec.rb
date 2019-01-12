3b9557efbcaf79ed481144a1c8ae34678affc31a
woodylucas <wlucas0311@gmail.com>
2019-01-11 19:03:15 -0500

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -19,4 +19,28 @@ describe 'navigate' do
     visit "/posts/#{@post.id}"
     expect(page).to have_css("p", text: "My post desc")
   end
+
+
+  # spec/features/post_spec.rb
+
+  require 'rails_helper'
+
+  describe 'navigate' do
+    before do
+      @post = Post.create(title: "My Post", description: "My post desc")
+    end
+
+    it 'loads the show page' do
+      visit "/posts/#{@post.id}"
+      expect(page.status_code).to eq(200)
+    end
+  end
+
+
+
+
+
+
+
+
 end
